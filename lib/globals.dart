0f5ba234ac84de1b0363c6526a93769ae781f937
Rita <rita.geleta@jediupc.com>
2021-03-07 18:59:21 +0100

    Add colors and globals

---
+++ lib/globals.dart
@@ -0,0 +1,6 @@
+import 'package:bookspace/utils/get_swatch.dart';
+import 'package:flutter/material.dart';
+
+Color primary = getMaterial(0xFF9be619);
+Color secondary = getMaterial(0xFF314211);
+Color tertiary = getMaterial(0xFF313211);
