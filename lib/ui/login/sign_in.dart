e45f36f375a183e88b3202b3d47804b82af5ee55
jaumearmen <jaumearmengol29@gmail.com>
2021-03-16 17:56:59 +0100

    1.0 Login

--- lib/ui/login/sign_in.dart
+++ lib/ui/login/sign_in.dart
@@ -14,48 +14,87 @@ class SignIn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xf3f3f3f3),
-      body: ListView(children: [
-        Container(
-          margin: EdgeInsets.fromLTRB(0, 40.0, 0, 0),
-          constraints: BoxConstraints.expand(
-            height: 200.0,
-          ),
-          child: Image(
-            image: AssetImage('assets/images/logo.png'),
-          ),
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 40.0, vertical: 0),
-          constraints: BoxConstraints.expand(height: 50),
-          child: TextField(
-            controller: usernameController,
-            keyboardType: TextInputType.text,
-            decoration: InputDecoration(
-                labelText: 'Nombre de Usuario',
-                fillColor: Colors.white,
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.person),
-                suffixIcon: IconButton(
-                    icon: Icon(Icons.close),
-                    onPressed: () => usernameController.clear())),
-          ),
-        ),
-        Container(
-            margin: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20),
-            constraints: BoxConstraints.expand(height: 50),
-            child: TextField(
-              controller: passwordController,
-              obscureText: isPasswordHiden,
-              decoration: InputDecoration(
-                  labelText: 'Contraseña',
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.person),
-                  suffixIcon: IconButton(
-                      icon: Icon(Icons.visibility),
-                      onPressed: () => _togglePasswordView())),
+      body: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 40.0, 0, 0),
+              constraints: BoxConstraints.expand(
+                height: 200.0,
+              ),
+              child: Image(
+                image: AssetImage('assets/images/logo.png'),
+              ),
+            ),
+            Container(
+              constraints: BoxConstraints.expand(height: 50),
+              child: TextField(
+                controller: usernameController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                    labelText: 'Nombre de Usuario',
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person),
+                    suffixIcon: IconButton(
+                        icon: Icon(Icons.close),
+                        onPressed: () => usernameController.clear())),
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.symmetric(horizontal: 0, vertical: 20),
+                constraints: BoxConstraints.expand(height: 50),
+                child: TextField(
+                  controller: passwordController,
+                  obscureText: isPasswordHiden,
+                  decoration: InputDecoration(
+                      labelText: 'Contraseña',
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.person),
+                      suffixIcon: IconButton(
+                          icon: Icon(Icons.visibility),
+                          onPressed: () => _togglePasswordView())),
+                )),
+            Container(
+                margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                child: ElevatedButton(
+                  onPressed: () => print('Inici de sessió amb google'),
+                  child: Text('Iniciar sesión con GOOGLE',
+                      style: TextStyle(
+                        color: Colors.black, /*fontFamily: "Schyler"*/
+                      )),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.blue[200], onPrimary: Colors.black),
+                )),
+            Container(
+                child: ElevatedButton(
+              onPressed: () => print('Inici de sessió'),
+              child: Text('Iniciar Sesión',
+                  style: TextStyle(
+                    color: Colors.black, /*fontFamily: "Schyler"*/
+                  )),
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.orangeAccent, onPrimary: Colors.black),
             )),
-      ]),
+            Container(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                  TextButton(
+                      onPressed: () => print('Anar a recupesrar contrasenya'),
+                      child: Text(
+                        '¿Olvidaste tu contrasenya?',
+                        style: TextStyle(color: Colors.black),
+                      )),
+                  TextButton(
+                      onPressed: () => print('Registrarse'),
+                      child: Text(
+                        'Registrarse',
+                        style: TextStyle(color: Colors.black),
+                      ))
+                ]))
+          ]),
     );
   }
 
