bc27391444ca71937bf1087711fa8d5d431ec1d8
Rita <rita.geleta@jediupc.com>
2021-03-23 22:19:33 +0100

    user view

--- lib/ui/profile/profile_view.dart
+++ lib/ui/profile/profile_view.dart
@@ -1,3 +1,5 @@
+import 'package:bookspace/controllers/user_controller.dart';
+import 'package:bookspace/models/user.dart';
 import 'package:flutter/material.dart';
 
 class ProfileView extends StatefulWidget {
@@ -8,10 +10,39 @@ class ProfileView extends StatefulWidget {
 }
 
 class _ProfileViewState extends State<ProfileView> {
+  User _user;
+
+  void getUser() async {
+    User user = await UserController.getUser(3);
+    if (!disposed){
+      setState(() => _user = user);
+    }
+    print(_user);
+  }
+
+  @override
+  void initState() { 
+    super.initState();
+    getUser();
+  }
+
+  bool disposed = false;
+  @override
+  void dispose() {
+    disposed = true;
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text('hello profile'),
-    );
+    if (_user != null) {
+      return Container(
+       child: Text('hello ${_user.username}'),
+      );
+    } else {
+      return Container(
+       child: Text('No hay user aun'),
+      );
+    }
   }
 }
