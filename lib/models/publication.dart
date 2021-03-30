4d3dd398014bc1ed63a41318e3200fc20e6aa069
Rita <rita.geleta@jediupc.com>
2021-03-30 18:30:28 +0200

    New api + publication model

---
+++ lib/models/publication.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+import 'package:intl/intl.dart';
+
+Publication publicationFromJson(String str) => Publication.fromJson(json.decode(str));
+
+String publicationToJson(Publication data) => json.encode(data.toJson());
+
+enum Category { ACTION, LOVE, WAR, POTENTIAL }
+
+extension CategoryExtension on Category {
+  int get category {
+    switch (this) {
+      case Category.ACTION:
+        return 0;
+      case Category.LOVE:
+        return 1;
+      case Category.WAR:
+        return 2;
+      case Category.POTENTIAL:
+        return 3;
+      default:
+        return null;
+    }
+  }
+}
+
+class Publication {
+  int id;
+  String title;
+  String content;
+  DateTime dop; // date of publication
+  //String author;
+
+  //List<String> votedBy;
+  //List<String> favoriteBy;
+  //List<String> comments;
+  //int category;
+
+
+  Publication({
+    this.id,
+    this.title,
+    this.content,
+    this.dop,
+    //this.author,
+    //this.votedBy,
+    //this.favoriteBy,
+    //this.comments,
+    //this.category,
+  });
+
+  factory Publication.fromJson(Map<String, dynamic> json) => Publication(
+        id: json["id"],
+        title: json["title"],
+        content: json["content"],
+        dop: DateTime.parse(json["dop"]),
+        //author: json["author"],
+        //description: json["description"],
+        //category: int.tryParse(json["category"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "content": content,
+        "dop": dop,
+        //"author": author,
+        //"category": category,
+      };
+}
+
