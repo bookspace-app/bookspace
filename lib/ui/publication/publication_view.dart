399f81cc2c99106bf676eca03c2a6fd3a3615b95
Rita <rita.geleta@jediupc.com>
2021-03-30 18:31:03 +0200

    get publication by id view

--- lib/ui/publication/publication_view.dart
+++ lib/ui/publication/publication_view.dart
@@ -1,3 +1,5 @@
+import 'package:bookspace/controllers/publication_controller.dart';
+import 'package:bookspace/models/publication.dart';
 import 'package:bookspace/ui/publication/widgets/publication_hero.dart';
 import 'package:bookspace/ui/publication/widgets/response_card.dart';
 import 'package:bookspace/ui/publication/widgets/user_card.dart';
@@ -15,6 +17,30 @@ class PublicationView extends StatefulWidget {
 }
 
 class _PublicationViewState extends State<PublicationView> {
+  Publication _publication;
+
+  void getPublication(int id) async {
+    Publication publication = await PublicationController.getPublication(id);
+    print('LA PUBLI');
+    print(publication);
+    if (!disposed){
+      setState(() => _publication = publication);
+    }
+    print(_publication);
+  }
+
+  @override
+  void initState() { 
+    super.initState();
+    getPublication(widget.id);
+  }
+
+  bool disposed = false;
+  @override
+  void dispose() {
+    disposed = true;
+    super.dispose();
+  }
   
   @override
   Widget build(BuildContext context) {
