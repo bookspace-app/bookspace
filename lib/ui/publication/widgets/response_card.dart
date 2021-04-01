59908ae1a089ce78e2f80908a00f03263858e90f
Rita <rita.geleta@jediupc.com>
2021-04-01 14:31:01 +0200

    A dummy response card

--- lib/ui/publication/widgets/response_card.dart
+++ lib/ui/publication/widgets/response_card.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 
-class ResponseDart extends StatefulWidget {
-  ResponseDart({Key key}) : super(key: key);
+class ResponseCard extends StatelessWidget {
+  ResponseCard({Key key}) : super(key: key);
 
-  @override
-  _ResponseDartState createState() => _ResponseDartState();
-}
-
-class _ResponseDartState extends State<ResponseDart> {
-  @override
   Widget build(BuildContext context) {
     return Container(
-       child: Text('A response'),
+       child: Container(
+           padding: EdgeInsets.all(10),
+            //color: Colors.blue[200],
+            child: Text(
+              "And even though it can be quite challenging and quite difficult to do these things, we have to make time. We have to find the strength and courage to be bring awareness into our lives and be true to ourselves, because if we don’t… if we don’t seek to find, to know and to understand the purpose of life and the reason of our existence, chances are that we will continue to live by default, betraying ourselves and thinking that it’s okay to walk on a path that is not ours to walk upon.",
+              style: TextStyle(),
+            )
+          )
     );
   }
 }
