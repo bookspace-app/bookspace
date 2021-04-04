db25d01348371099f9c1a1b45650e5512c0d778b
jaumearmen <jaumearmengol29@gmail.com>
2021-04-05 01:48:29 +0200

    versio 1.1 edit_profile (abans d'anar a dormir)

--- lib/ui/profile/edit_profile_view.dart
+++ lib/ui/profile/edit_profile_view.dart
@@ -1,3 +1,4 @@
+import 'package:bookspace/ui/profile/profile_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -19,6 +20,13 @@ class _EditProfileViewState extends State<EditProfileView> {
   final passwordController = TextEditingController();
   final emailController = TextEditingController();
   final bioController = TextEditingController();
+
+  var _name;
+  var _surname;
+  var _username;
+  var _password;
+  var _email;
+  var _bio;
 
   @override
   Widget build(BuildContext context) {
@@ -60,21 +68,35 @@ class _EditProfileViewState extends State<EditProfileView> {
             ),
           ),
           Container(
-            constraints: BoxConstraints.expand(height: 40, width: 100),
-            child: TextField(
-              controller: nameController,
-              decoration: InputDecoration(
-                  hintText: 'Nombre',
-                  border: OutlineInputBorder(),
+              constraints: BoxConstraints.expand(height: 50, width: 100),
+              child: TextField(
+                controller: nameController,
+                style: TextStyle(
+                    fontSize: 15,
+                    //color: Color(0xff0962ff),
+                    fontWeight: FontWeight.bold),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  hintText: 'Jaume',
+                  hintStyle: TextStyle(
+                      fontSize: 15,
+                      color: Colors.grey[350],
+                      fontWeight: FontWeight.w600),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
                   suffixIcon: IconButton(
                       icon: Icon(Icons.clear),
                       onPressed: () {
                         setState(() {
                           nameController.clear();
                         });
-                      })),
-            ),
-          ),
+                      }),
+                ),
+              )),
           Container(
             margin: EdgeInsets.fromLTRB(0, 15, 0, 5),
             child: Text(
@@ -85,21 +107,35 @@ class _EditProfileViewState extends State<EditProfileView> {
             ),
           ),
           Container(
-            constraints: BoxConstraints.expand(height: 40, width: 100),
-            child: TextField(
-              controller: surnameController,
-              decoration: InputDecoration(
-                  hintText: 'Apellidos',
-                  border: OutlineInputBorder(),
+              constraints: BoxConstraints.expand(height: 50, width: 100),
+              child: TextField(
+                controller: surnameController,
+                style: TextStyle(
+                    fontSize: 15,
+                    //color: Color(0xff0962ff),
+                    fontWeight: FontWeight.bold),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  hintText: 'Armengol Tapiolas',
+                  hintStyle: TextStyle(
+                      fontSize: 15,
+                      color: Colors.grey[350],
+                      fontWeight: FontWeight.w600),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
                   suffixIcon: IconButton(
                       icon: Icon(Icons.clear),
                       onPressed: () {
                         setState(() {
                           surnameController.clear();
                         });
-                      })),
-            ),
-          ),
+                      }),
+                ),
+              )),
           Container(
             margin: EdgeInsets.fromLTRB(0, 15, 0, 5),
             child: Text(
@@ -110,21 +146,35 @@ class _EditProfileViewState extends State<EditProfileView> {
             ),
           ),
           Container(
-            constraints: BoxConstraints.expand(height: 40, width: 100),
-            child: TextField(
-              controller: usernameController,
-              decoration: InputDecoration(
-                  hintText: 'Nombre de usuario',
-                  border: OutlineInputBorder(),
+              constraints: BoxConstraints.expand(height: 50, width: 100),
+              child: TextField(
+                controller: usernameController,
+                style: TextStyle(
+                    fontSize: 15,
+                    //color: Color(0xff0962ff),
+                    fontWeight: FontWeight.bold),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  hintText: 'jaumearmen',
+                  hintStyle: TextStyle(
+                      fontSize: 15,
+                      color: Colors.grey[350],
+                      fontWeight: FontWeight.w600),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
                   suffixIcon: IconButton(
                       icon: Icon(Icons.clear),
                       onPressed: () {
                         setState(() {
                           usernameController.clear();
                         });
-                      })),
-            ),
-          ),
+                      }),
+                ),
+              )),
           Container(
             margin: EdgeInsets.fromLTRB(0, 15, 0, 5),
             child: Text(
@@ -135,21 +185,35 @@ class _EditProfileViewState extends State<EditProfileView> {
             ),
           ),
           Container(
-            constraints: BoxConstraints.expand(height: 40, width: 100),
-            child: TextField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                  hintText: 'Contraseña',
-                  border: OutlineInputBorder(),
+              constraints: BoxConstraints.expand(height: 50, width: 100),
+              child: TextField(
+                controller: passwordController,
+                style: TextStyle(
+                    fontSize: 15,
+                    //color: Color(0xff0962ff),
+                    fontWeight: FontWeight.bold),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  hintText: 'Password',
+                  hintStyle: TextStyle(
+                      fontSize: 15,
+                      color: Colors.grey[350],
+                      fontWeight: FontWeight.w600),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
                   suffixIcon: IconButton(
                       icon: Icon(Icons.clear),
                       onPressed: () {
                         setState(() {
                           passwordController.clear();
                         });
-                      })),
-            ),
-          ),
+                      }),
+                ),
+              )),
           Container(
             margin: EdgeInsets.fromLTRB(0, 15, 0, 5),
             child: Text(
@@ -160,21 +224,35 @@ class _EditProfileViewState extends State<EditProfileView> {
             ),
           ),
           Container(
-            constraints: BoxConstraints.expand(height: 40, width: 100),
-            child: TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                  hintText: 'Email',
-                  border: OutlineInputBorder(),
+              constraints: BoxConstraints.expand(height: 50, width: 100),
+              child: TextField(
+                controller: emailController,
+                style: TextStyle(
+                    fontSize: 15,
+                    //color: Color(0xff0962ff),
+                    fontWeight: FontWeight.bold),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  hintText: 'jaume29at@gmail.com',
+                  hintStyle: TextStyle(
+                      fontSize: 15,
+                      color: Colors.grey[350],
+                      fontWeight: FontWeight.w600),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
                   suffixIcon: IconButton(
                       icon: Icon(Icons.clear),
                       onPressed: () {
                         setState(() {
                           emailController.clear();
                         });
-                      })),
-            ),
-          ),
+                      }),
+                ),
+              )),
           MyCustomInputBox(
             label: 'Biografia',
             inputHint: 'Hola',
@@ -206,13 +284,66 @@ class _EditProfileViewState extends State<EditProfileView> {
             ),
           ),
           Container(
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Tags',
-                border: OutlineInputBorder(),
-              ),
-            ),
-          )
+              constraints: BoxConstraints.expand(height: 50, width: 100),
+              child: TextField(
+                //controller: emailController,
+                style: TextStyle(
+                    fontSize: 15,
+                    //color: Color(0xff0962ff),
+                    fontWeight: FontWeight.bold),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  hintText: 'Space & Aliens',
+                  hintStyle: TextStyle(
+                      fontSize: 15,
+                      color: Colors.grey[350],
+                      fontWeight: FontWeight.w600),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  /*suffixIcon: IconButton(
+                      icon: Icon(Icons.clear),
+                      onPressed: () {
+                        setState(() {
+                          tagsController.clear();
+                        });
+                      }),*/
+                ),
+              )),
+          Container(
+              alignment: Alignment.bottomCenter,
+              height: 40,
+              margin: EdgeInsets.only(top: 20),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _name = nameController.text;
+                    _surname = surnameController.text;
+                    _username = usernameController.text;
+                    _email = emailController.text;
+                    _password = passwordController.text;
+                    _bio = bioController.text;
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ProfileView()),
+                    );
+                  });
+                },
+                child: Text('Submit',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black, /*fontFamily: "Schyler"*/
+                    )),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.grey,
+                    onPrimary: Colors.black,
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(5.0))),
+              )),
         ],
       ),
     );
--- lib/ui/widgets/custom_input_box.dart
+++ lib/ui/widgets/custom_input_box.dart
@@ -19,45 +19,44 @@ class _MyCustomInputBoxState extends State<MyCustomInputBox> {
         Align(
           alignment: Alignment.centerLeft,
           child: Padding(
-            padding: const EdgeInsets.only(left: 0.0, bottom: 8),
+            padding: const EdgeInsets.fromLTRB(0, 15, 0, 5),
             child: Text(
               widget.label,
               style: TextStyle(
                 fontFamily: 'Product Sans',
                 fontSize: 15,
-                color: Color(0xff8f9db5),
+                //color: Color(0xff8f9db5),
               ),
             ),
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(40, 0, 40, 15),
-          child: TextFormField(
-            obscureText: widget.label == 'Contraseña' ? true : false,
-            style: TextStyle(
-                fontSize: 19,
-                color: Color(0xff0962ff),
-                fontWeight: FontWeight.bold),
-            decoration: InputDecoration(
-                hintText: widget.inputHint,
-                hintStyle: TextStyle(
-                    fontSize: 18,
-                    color: Colors.grey[350],
-                    fontWeight: FontWeight.w600),
-                focusColor: Color(0xff0962ff),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: BorderSide(color: Color(0xff0962ff)),
-                ),
-                suffixIcon: IconButton(
-                    icon: Icon(Icons.clear),
-                    onPressed: () {
-                      setState(() {
-                        controller.clear();
-                      });
-                    })),
-          ),
-        )
+        TextFormField(
+          obscureText: widget.label == 'Contraseña' ? true : false,
+          style: TextStyle(
+              fontSize: 15,
+              //color: Color(0xff0962ff),
+              fontWeight: FontWeight.bold),
+          decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              hintText: widget.inputHint,
+              hintStyle: TextStyle(
+                  fontSize: 15,
+                  color: Colors.grey[350],
+                  fontWeight: FontWeight.w600),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black),
+              ),
+              suffixIcon: IconButton(
+                  icon: Icon(Icons.clear),
+                  onPressed: () {
+                    setState(() {
+                      controller.clear();
+                    });
+                  })),
+        ),
       ],
     );
   }
