90e68c61e4fb4374f0289342c1ef89956fc8ddc5
Alvarobf0 <alvarobofo@gmail.com>
2021-04-13 01:08:17 +0200

    view

--- lib/ui/chat/chat_list_view.dart
+++ lib/ui/chat/chat_list_view.dart
@@ -1,4 +1,8 @@
+import 'package:bookspace/controllers/user_controller.dart';
+import 'package:bookspace/models/user.dart';
 import 'package:flutter/material.dart';
+
+
 
 class ChatListView extends StatefulWidget {
   ChatListView({Key key}) : super(key: key);
@@ -8,10 +12,57 @@ class ChatListView extends StatefulWidget {
 }
 
 class _ChatListViewState extends State<ChatListView> {
+  User _user;
+  List<Chat> _myChats = [];
+
+  void getUser() async {
+    User user = await UserController.getUser(1);
+    if (!disposed){
+      setState(() => _user = user);
+    }
+  }
+
+  @override
+  void initState() { 
+    super.initState();
+    getUser();
+  }
+
+  bool disposed = false;
+  @override
+  void dispose() {
+    disposed = true;
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text('hello chat'),
-    );
+    return Container(child: ListView(
+      children: [
+        for (var index = 0; index < _myChats.length; index++) Column(            //TO-DO Add left padding (15) & right padding (?) to publication cards
+          children: <Widget>[
+            Container(height: (index == 0)?10:0),
+            InkWell(
+              child: ChatCard(
+                publication: _myChats[index]
+              ),
+              onTap: () {
+                Navigator.push(
+                  context, // TODO: pass id to PublicationView
+                  MaterialPageRoute(
+                    builder: (context) => MainView(
+                      renderIndex: 'chatList',
+                      view: ChatView(id: _myChats[index].id),
+                    )
+                  ),
+                );
+              }, // on tap llevar a la view de la publicacion
+            ), 
+            Divider() 
+          ]
+        )
+      ],
+    ));
   }
 }
