3096eb680e542f54ac59ed93291d324e862ae177
Rita <noticiasmundiales99@gmail.com>
2021-05-03 20:40:24 +0200

    create comment widget

--- lib/ui/publication/widgets/create_comment.dart
+++ lib/ui/publication/widgets/create_comment.dart
@@ -1,3 +1,4 @@
+import 'package:bookspace/app_localizations.dart';
 import 'package:bookspace/controllers/comment_controller.dart';
 import 'package:bookspace/controllers/user_controller.dart';
 import 'package:bookspace/models/comment.dart';
@@ -5,14 +6,17 @@ import 'package:bookspace/models/user.dart';
 import 'package:flutter/material.dart';
 
 import 'package:bookspace/globals.dart' as globals;
+import 'package:flutter/services.dart';
 
 class CreateComment extends StatefulWidget {
 
   final int id;
+  final Function() notifyOnNewComment;
 
   CreateComment({
     Key key,
-    this.id
+    this.id,
+    @required this.notifyOnNewComment
   }) : super(key: key);
 
   @override
@@ -23,6 +27,8 @@ class _CreateCommentState extends State<CreateComment> {
   User _user;
   Comment myComment;
   var contentController = TextEditingController();
+
+  bool errorsContent = false;
 
   void getUser() async {
     User user =
@@ -41,7 +47,7 @@ class _CreateCommentState extends State<CreateComment> {
     super.dispose();
   }
 
-  void createPublication() async {
+  void createComment() async {
     int response = await CommentController.createComment(myComment);
     print(globals.id);
     /*
@@ -56,11 +62,86 @@ class _CreateCommentState extends State<CreateComment> {
     */
   }
 
-  
+  String errorContent() {
+    if (contentController.text.isEmpty)
+      return "${AppLocalizations.of(context).translate("noContent")}";
+    else if (contentController.text.length > 2000)
+      return "${AppLocalizations.of(context).translate("longContent")}";
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-       child: child,
+      color: Colors.amber,
+      padding: EdgeInsets.fromLTRB(15, 2, 15, 5),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextFormField(
+              controller: contentController,
+              onChanged: (text) {
+                setState(() {});
+              },
+              keyboardType: TextInputType.multiline,
+              maxLines: 4,
+              inputFormatters: [
+                new LengthLimitingTextInputFormatter(2000),
+              ], //TO-DO Define length of publication's description
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(),
+                counterText:
+                  '${contentController.text.length}' + '/2000',
+                contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                errorText: errorsContent ? errorContent() : null,
+              )
+            )  
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(15, 0, 15, 5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center, //Center Row contents horizontally,
+              crossAxisAlignment: CrossAxisAlignment.center, //Center Row contents vertically,
+              children: <Widget>[
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.30,
+                  height: 35,
+                  child: RaisedButton(
+                    textColor: Colors.white,
+                    color: Color.fromRGBO(250, 198, 65, 1),
+                    child: Text(
+                      'Publicar',
+                      style: TextStyle(
+                        fontSize: 18.0, color: Colors.black),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          errorsContent = (errorContent() != null) ? true : false;
+                        });
+                        print(errorsContent);
+                        if (!errorsContent) {
+                          List<int> mentionIds = [];
+                          print(contentController.text);
+                          print(contentController.text.toString());
+                          myComment = Comment();
+                          myComment.authorId = (globals.id).toInt();
+                          myComment.content = contentController.text;
+                          myComment.mentions = mentionIds;
+                          myComment.parentId = null;
+                          myComment.publicationId = (widget.id).toInt();
+                          createComment();
+                          widget.notifyOnNewComment();
+                        }
+                      }
+                    )
+                )
+              ],
+            ), 
+          )
+        ]
+      )
     );
   }
 }
